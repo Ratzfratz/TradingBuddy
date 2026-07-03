db20719e0cf176b6fb9ab180dda448fe79348229
Ratzfratz <Illmer.michael.87@gmail.com>
2026-07-03 23:12:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,78 @@
+const CACHE = 'warrantcalc-v49';
+
+const ASSETS = [
+  '/app.html',
+  '/manifest.json',
+  '/favicon.ico',
+  '/icons/icon-16.png',
+  '/icons/icon-32.png',
+  '/icons/apple-touch-icon.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches
+      .open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (event.request.mode === 'navigate' || url.pathname === '/app.html') {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          if (response.status === 200) {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(response => response || caches.match('/app.html')))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+          if (
+            event.request.method === 'GET' &&
+            response.status === 200
+          ) {
+            const copy = response.clone();
+
+            caches
+              .open(CACHE)
+              .then(cache => cache.put(event.request, copy));
+          }
+
+          return response;
+        })
+        .catch(() => caches.match('/app.html'));
+    })
+  );
+});
