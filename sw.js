09f9c86ff6dfe5bfccd367b19c8d16ab2e9d5b4e
Rooch <rooch@users.noreply.github.com>
2026-07-07 11:37:06 +0200

    Remove small tag sample label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'warrantcalc-v77';
+const CACHE = 'warrantcalc-v78';
 const SCOPE_URL = new URL(self.registration.scope);
 const scopePath = SCOPE_URL.pathname.replace(/\/$/, '');
 const appPath = path => `${scopePath}${path}`;
