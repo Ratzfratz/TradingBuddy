1f1e07f4223c6f348ce4726ec7ea3c8ab8f68d88
Rooch <rooch@users.noreply.github.com>
2026-07-07 11:08:14 +0200

    Detect KO short imports correctly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'warrantcalc-v72';
+const CACHE = 'warrantcalc-v73';
 const SCOPE_URL = new URL(self.registration.scope);
 const scopePath = SCOPE_URL.pathname.replace(/\/$/, '');
 const appPath = path => `${scopePath}${path}`;
