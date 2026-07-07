4c8502521f0cbd2a1481a54d2601231b1397d4ca
Rooch <rooch@users.noreply.github.com>
2026-07-07 10:18:56 +0200

    Prepare GitHub Pages hosting

--- sw.js
+++ sw.js
@@ -1,15 +1,25 @@
-const CACHE = 'warrantcalc-v64';
+const CACHE = 'warrantcalc-v65';
+const SCOPE_URL = new URL(self.registration.scope);
+const scopePath = SCOPE_URL.pathname.replace(/\/$/, '');
+const appPath = path => `${scopePath}${path}`;
+const appUrl = path => new URL(appPath(path), self.location.origin).toString();
 
 const ASSETS = [
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico',
-  '/icons/icon-16.png',
-  '/icons/icon-32.png',
-  '/icons/apple-touch-icon.png',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  appUrl('/index.html'),
+  appUrl('/manifest.json'),
+  appUrl('/favicon.ico'),
+  appUrl('/icons/icon-16.png'),
+  appUrl('/icons/icon-32.png'),
+  appUrl('/icons/apple-touch-icon.png'),
+  appUrl('/icons/icon-192.png'),
+  appUrl('/icons/icon-512.png')
 ];
+
+const INDEX_URL = appUrl('/index.html');
+const isAppIndex = url => url.origin === self.location.origin && (
+  url.pathname === appPath('/') ||
+  url.pathname === appPath('/index.html')
+);
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -36,10 +46,29 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   const url = new URL(event.request.url);
-  if (event.request.mode === 'navigate' || url.pathname === '/index.html') {
+  if (event.request.mode === 'navigate' || isAppIndex(url)) {
     event.respondWith(
       fetch(event.request)
+        .then(response => {
+          if (response.status === 200) {
+            const copy = response.clone();
+            caches.open(CACHE).then(cache => cache.put(INDEX_URL, copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(response => response || caches.match(INDEX_URL)))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
         .then(response => {
           if (response.status === 200) {
             const copy = response.clone();
@@ -47,32 +76,7 @@ self.addEventListener('fetch', event => {
           }
           return response;
         })
-        .catch(() => caches.match(event.request).then(response => response || caches.match('/index.html')))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then(response => {
-          if (
-            event.request.method === 'GET' &&
-            response.status === 200
-          ) {
-            const copy = response.clone();
-
-            caches
-              .open(CACHE)
-              .then(cache => cache.put(event.request, copy));
-          }
-
-          return response;
-        })
-        .catch(() => caches.match('/index.html'));
+        .catch(() => caches.match(INDEX_URL));
     })
   );
 });
