26bf50e0769565585a918a086d1157c073976ac3
Rooch <rooch@users.noreply.github.com>
2026-07-07 10:47:38 +0200

    Deduplicate repeated Zero import rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'warrantcalc-v67';
+const CACHE = 'warrantcalc-v68';
 const SCOPE_URL = new URL(self.registration.scope);
 const scopePath = SCOPE_URL.pathname.replace(/\/$/, '');
 const appPath = path => `${scopePath}${path}`;
