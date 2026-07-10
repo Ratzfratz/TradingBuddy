4a557a68af4213fbd1e11edd6691cca5e5c908f6
Ratzfratz <illmer.michael.87@gmail.com>
2026-07-10 08:36:22 +0200

    Fix cloud screenshot loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'warrantcalc-v138';
+const CACHE = 'warrantcalc-v139';
 const SCOPE_URL = new URL(self.registration.scope);
 const scopePath = SCOPE_URL.pathname.replace(/\/$/, '');
 const appPath = path => `${scopePath}${path}`;
